70c328146de84082f20a99f8c29f754ea0978c08
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2021-07-30 23:22:02 -0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Andres Marquez on 2021-07-30.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
